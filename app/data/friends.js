f377ec4c7c8e1169ebb59779c8f5980e69be0d8b
riffon3000 <riffon@gmail.com>
2018-11-11 19:14:03 -0600

    add picture links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 const friends = [
     {
       "name": "Ross",
-      "photo": "./photos/ross.jpg",
+      "photo": "https://github.com/riffon3000/FriendFinder/blob/master/app/data/photos/ross.jpg",
       "scores": ["1","5","5","3","1","5","5","4","5","5"]
     },
     {
         "name": "Joey",
-        "photo": "./photos/joey.jpg",
+        "photo": "https://github.com/riffon3000/FriendFinder/blob/master/app/data/photos/joey.jpg",
         "scores": ["4","2","5","1","3","2","2","1","3","2"]
     },
     {
         "name": "Chandler",
-        "photo": "./photos/chandler.jpg",
+        "photo": "https://github.com/riffon3000/FriendFinder/blob/master/app/data/photos/chandler.jpg",
         "scores": ["5","2","2","2","4","1","3","2","5","5"]
     },
     {
         "name": "Phoebe",
-        "photo": "./photos/phoebe.jpg",
+        "photo": "https://github.com/riffon3000/FriendFinder/blob/master/app/data/photos/phoebe.jpg",
         "scores": ["3","3","4","2","2","1","3","2","2","3"]
     },
     {
         "name": "Monica",
-        "photo": "./photos/monica.jpg",
+        "photo": "https://github.com/riffon3000/FriendFinder/blob/master/app/data/photos/monica.jpg",
         "scores": ["5","1","4","4","5","1","2","5","4","1"]
     },
     {
         "name": "Rachel",
-        "photo": "./photos/rachel.jpg",
+        "photo": "https://github.com/riffon3000/FriendFinder/blob/master/app/data/photos/rachel.jpg",
         "scores": ["2","3","3","3","5","3","4","3","2","4"]
     }
   ];
